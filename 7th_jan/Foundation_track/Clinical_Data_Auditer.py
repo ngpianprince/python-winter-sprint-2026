ebe99c61d75a3a96a30146cc5d9601b725f16ce5
ngpianprince <ngpianprince@gmail.com>
2026-01-11 22:11:00 +0530

    Create Clinical_Data_Auditer.py

---
+++ 7th_jan/Foundation_track/Clinical_Data_Auditer.py
@@ -0,0 +1,16 @@
+# Clinical Data Record
+
+patient_name = "Raj"
+age = 18
+heart_rate = 72
+city = "New Delhi"
+
+patient_name = patient_name.title()
+city = city.upper()
+
+print("------ Clinical Data Record ------")
+print(f"Patient Name : {patient_name}")
+print(f"Age          : {age}")
+print(f"Heart Rate   : {heart_rate} bpm")
+print(f"City         : {city}")
+print("----------------------------------")
